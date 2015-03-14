b6828fca3858d75f0608e684ffea3f29814a4798
Ned Hyett <nedhyett@users.noreply.github.com>
2015-03-14 18:02:05 +0000

    Update full_example.lua

--- vermilion2/lua/vermilion2/modules/full_example.lua
+++ vermilion2/lua/vermilion2/modules/full_example.lua
@@ -1,23 +1,4 @@
 --[[
- Copyright 2015 Ned Hyett, 
-
- Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- in compliance with the License. You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software distributed under the License
- is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- or implied. See the License for the specific language governing permissions and limitations under
- the License.
-
- The right to upload this project to the Steam Workshop (which is operated by Valve Corporation)
- is reserved by the original copyright holder, regardless of any modifications made to the code,
- resources or related content. The original copyright holder is not affiliated with Valve Corporation
- in any way, nor claims to be so.
-]]
-
---[[
 	This file will never be loaded by Vermilion and is here to serve as a reference file for all the possible properties that Vermilion
 	will be looking for in a module file.
 ]]--
@@ -37,10 +18,13 @@ MODULE.Permissions = { -- a list of permissions that Vermilion should load. Note
 	"your_permission_name_here",
 	"my_new_permission"
 }
+MODULE.NetworkStrings = { -- list of message names to send to and from the server, only has to be unique inside the module
+	"MyNetworkString" 
+}
 
 
 function MODULE:InitServer()
-
+	
 end
 
 function MODULE:InitClient()
